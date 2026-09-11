d5e9c7effb1db68f2ae0b42247c0c120e22c908e
GreatDavid <91980476+GreatKingdavid@users.noreply.github.com>
2026-09-11 04:27:53 +0300

    Revert "git push origin feat/channels-creation-and--messaging"

--- src/messages/dto/message-response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class MessageResponseDto {
-  @ApiProperty() id: string;
-  @ApiProperty() channelId: string;
-  @ApiProperty() senderId: string;
-  @ApiProperty() content: string;
-  @ApiProperty() createdAt: Date;
-}
