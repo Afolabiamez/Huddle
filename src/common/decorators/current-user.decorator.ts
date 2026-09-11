efbf929fdbeece7a7cea9f37abd9e4e38f8105e4
James Afolabi <afolabijames1997@gmail.com>
2026-09-11 11:53:44 +0100

    chore: permanently delete duplicate backend files from root directory

--- src/common/decorators/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { AuthenticatedRequest } from '../../auth/jwt-auth.guard.js';
-
-export const CurrentUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest<AuthenticatedRequest>();
-    return request.user;
-  },
-);
--- src/messages/dto/create-message.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, Length } from 'class-validator';
-
-export class CreateMessageDto {
-  @ApiProperty({ example: 'Hey team, standup in 5!' })
-  @IsString()
-  @Length(1, 4000)
-  content: string;
-}
